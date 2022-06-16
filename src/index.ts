925957ca64066ac49856e0675dc062e4e7da1fae
ajosemalik <ajose.malik@gmail.com>
2022-06-15 21:28:32 -0400

    refactor with static method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import {MatchReader} from './MatchReader'
 import {CsvFileReader} from './CsvFileReader'
-import {ConsoleReport} from './reportTargets/ConsoleReport'
 import {WinsAnalysis} from './analyzers/WinsAnalysis'
 import {Summary} from './Summary'
 import {HtmlReport} from './reportTargets/HtmlReport'
@@ -9,9 +8,6 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new HtmlReport()
-)
+const summary = Summary.winsAnalysisWithhtmlReport('Man United')
 
 summary.buildAndPrintReport(matchReader.matches);
